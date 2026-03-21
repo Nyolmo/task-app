dc0202b58ae87300cc580914470d04a2a6b0813d
Nyolmo <nyolmorodney@gmail.com>
2026-03-21 20:48:59 +0200

    feat(frontend): add task creation with POST request
    - implemented form for adding new tasks
    - added handleSubmit function to send POST request to backend
    - handled loading and error states
    - updated UI to display newly created task instantly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,51 @@ function App(){
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState({title:"", description:""});
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(()=>{
     fetchTasks();
   }, []);
 
+  const handleChange = (e)=>{
+    setNewTask({...newTask, [e.target.name]:e.target.value});
+  }
 
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  // Validate
+  if (!newTask.title.trim()) {
+    setError("Title is required");
+    return;
+  }
+
+  setError("");
+  setLoading(true);
+
+  try {
+    const res = await fetch("http://localhost:5000/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask), 
+    });
+
+    if (!res.ok) throw new Error("Failed to create task");
+
+    const createdTask = await res.json(); 
+
+    // Update UI immediately
+    setTasks([createdTask, ...tasks]);
+    setNewTask({ title: "", description: "" });
+  } catch (err) {
+    console.error(err);
+    setError("Error creating task");
+  } finally {
+    setLoading(false);
+  }
+};
   const fetchTasks = async() =>{
     try{
       const res = await fetch('http://localhost:5000/api/tasks');
@@ -25,25 +64,63 @@ function App(){
 
 
   return(
-    <div className="p-6 bg-gray-300">
-      <h1 className="text-3xl text-black text-center font-bold">Task App</h1>
+    <div className="p-6 bg-gray-300 ">
+      <h1 className="text-3xl text-black text-center font-bold mb-4">Task App</h1>
+
+      {error && <p className="text-red-500 mb-2">{error}</p>}
+      <div >
+        <form onSubmit={handleSubmit} className="mb-6 flex flex-col gap-2" >
+          <input
+            type="text"
+            name="title"
+            placeholder="Task Title"
+            value={newTask.title}
+            onChange={handleChange}  
+            required 
+            className="border p-2 rounded-2xl"       
+          ></input>
+          <input
+            type="text"
+            name="description"
+            placeholder="Task Description"
+            value={newTask.description}
+            onChange={handleChange}  
+            required 
+            className="border rounded-2xl p-2"          
+          >
+          </input>
+
+          <button type="submit" onSubmit={handleSubmit} disabled={loading}
+            className="border rounded bg-blue-400 text-white hover:bg-blue-600 disabled:bg-gray-500"
+          
+          >{loading? "Creating..." : "Add Task"}</button>
+        </form>
+      </div>
       <div className="columns-3 py-6 mt-2 bg-black rounded-2xl text-white text-2xl font-bold px-6">
         <h1>Title</h1>
         <h1>Description</h1>
         <h1>Completed</h1>
       </div>
-      {
+      {tasks.length === 0 ? (
+        <p>No task Found</p>
+      ):(
+      
         tasks.map((task)=>(
-          <div key={task._id} className="px-6 text-white columns-3 py-6 mt-2 bg-black rounded-2xl" >
+          <div key={task._id} className="px-6 text-white columns-3 py-6 mt-2 bg-black  border p-3 rounded mb-2 shadow-sm" >
            
-            <h1 className="font-bold"><span>{task.title}</span></h1>
+            <h1 className="font-semibold"><span>{task.title}</span></h1>
             <p> {task.description}</p>
-            <p> {task.completed? "Completed✅" : "Not Completed ❌" }</p>
+            <p className="text-sm text-gray-500"> {task.completed? "Completed✅" : "Not Completed ❌" }</p>
           </div>
+          
         ))
-      }
+      
+  )}
+
     </div>
-  )
+  );
 }
+
+
 
 export default App;
